1463949847412e62f4c8059f216304405ee6b5d2
Andrea Bedei <andrea.bedei2@studio.unibo.it>
2025-04-25 09:44:58 +0200

    Refactor telemetry and graphics handling in test script; enhance frame display and gamepad interaction

--- test_all.py
+++ test_all.py
@@ -1,32 +1,32 @@
+import vgamepad as vg
+import cv2
+import time
+
 from env.shared_memory_physics import read_telemetry
 from vision.screen_capture import capture_screen
 from control.controller import send_action
 from env.shared_memory_graphics import read_graphics
-from utils.classify_vehicle_behavior import classify_vehicle_state
+from data.restart import restart_game
 
-import cv2
-import time
+gamepad=vg.VX360Gamepad()
+
 
 if __name__ == "__main__":
     print("[Test] Lettura dati e visualizzazione live")
-    # cv2.namedWindow("Assetto POV", cv2.WINDOW_NORMAL)
+    cv2.namedWindow("Assetto POV", cv2.WINDOW_NORMAL)
     for _ in range(10000):
+        
         telem = read_telemetry()
-        print("Telemetry:", telem)
         graphics = read_graphics()
-        print("Graphics:", graphics)
-        result = classify_vehicle_state(
-            telem["steer"], telem["speed"], telem["g_force"],
-            telem["wheel_slip"], telem["yaw_rate"], telem["gas"]
-        )
-        print("Classificazione:", result)
 
-        # frame = capture_screen()
-        # cv2.imshow("Assetto POV", frame)
-
-        # send_action([0.0, 0.5, 0.0])  # Sterzo 0, Gas 50%, Freno 0%
+        frame = capture_screen()
+        cv2.imshow("Assetto POV", frame)
+        send_action([0.0, 1, 0.0], gamepad)  # Sterzo 0, Gas 50%, Freno 0%
 
         if cv2.waitKey(1) == 27:
             break
-        time.sleep(0.5)
+        time.sleep(3)
+
+        restart_game()
+        
     cv2.destroyAllWindows()
