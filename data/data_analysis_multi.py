7b8b5efaaea9eaa226f149d98106fb8d2383bebd
Andrea Bedei <andrea.bedei2@studio.unibo.it>
2025-05-05 19:00:22 +0200

    Fix datasetName

--- data/data_analysis_multi.py
+++ data/data_analysis_multi.py
@@ -4,8 +4,6 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
-from sklearn.preprocessing import StandardScaler
-from sklearn.decomposition import PCA
 
 def parse_filename(filename):
     # Exemple: vehicle_telemetry_mattia_abu_0G_S.csv
@@ -30,11 +28,15 @@ def load_telemetry_data(folder_pattern="*.csv"):
     return pd.concat(rows, ignore_index=True) if rows else pd.DataFrame()
 
 def analyze_data(df):
+    df["wheel_slip_front_left"] = np.where(df["wheel_slip_front_left"] > 1, 1, df["wheel_slip_front_left"])
+    df["wheel_slip_rear_left"] = np.where(df["wheel_slip_rear_left"] > 1, 1, df["wheel_slip_rear_left"])
+    df["wheel_slip_front_right"] = np.where(df["wheel_slip_front_right"] > 1, 1, df["wheel_slip_front_right"])
+    df["wheel_slip_rear_right"] = np.where(df["wheel_slip_rear_right"] > 1, 1, df["wheel_slip_rear_right"])
+
     # Mean grip for driver and temperature
     slip_by_temp = df.groupby(['temp'])[['wheel_slip_front_left', 'wheel_slip_front_right',
                                         'wheel_slip_rear_left', 'wheel_slip_rear_right']].mean()
     slip_by_temp = slip_by_temp.mean(axis=1).reset_index(name='mean_slip')
-    slip_by_temp["mean_slip"] = np.where(slip_by_temp["mean_slip"] > 2, 2, slip_by_temp["mean_slip"])
 
 
     plt.figure(figsize=(10, 6))
@@ -54,7 +56,6 @@ def analyze_data(df):
     grouped_slip = df.groupby(['track', 'temp', 'driver'])[['wheel_slip_front_left', 'wheel_slip_front_right',
                                                             'wheel_slip_rear_left', 'wheel_slip_rear_right']].mean()
     grouped_slip = grouped_slip.mean(axis=1).reset_index(name='mean_slip')
-    grouped_slip["mean_slip"] = np.where(grouped_slip["mean_slip"] > 2, 2, grouped_slip["mean_slip"])
     plt.figure(figsize=(10, 6))
     sns.barplot(data=grouped_slip, x='track', y='mean_slip', hue='temp')
     plt.title("Average Slip by Track and Temperature")
@@ -72,7 +73,6 @@ def analyze_data(df):
     grip_behavior = df.groupby(['track', 'driver'])[['wheel_slip_front_left', 'wheel_slip_front_right',
                                                      'wheel_slip_rear_left', 'wheel_slip_rear_right']].mean()
     grip_behavior = grip_behavior.mean(axis=1).reset_index(name='mean_grip')
-    grip_behavior["mean_grip"] = np.where(grip_behavior["mean_grip"] > 2, 2, grip_behavior["mean_grip"])
     plt.figure(figsize=(10, 6))
     sns.barplot(data=grip_behavior, x='track', y='mean_grip', hue='driver')
     plt.title("Average grip behavior by track and driver")
@@ -107,7 +107,6 @@ def analyze_data(df):
     # Limiting the maximum value of slip to 3 for better visualization
     plt.figure(figsize=(12, 6))
     df["mean_slip_1"] = df[['wheel_slip_front_left', 'wheel_slip_front_right', 'wheel_slip_rear_left', 'wheel_slip_rear_right']].mean(axis=1)
-    df["mean_slip_1"] = np.where(df["mean_slip_1"] > 2, 2, df["mean_slip_1"])
     sns.boxplot(data=df, x='temp', y='mean_slip_1', hue='driver')
     plt.title("Distribution of average slip by temperature and driver")
     plt.xlabel("Temperature")
@@ -116,7 +115,40 @@ def analyze_data(df):
     plt.show()
 
 
-    
+        # Calcolo slip medio per ogni riga
+    df["mean_slip_1"] = df[['wheel_slip_front_left', 'wheel_slip_front_right',
+                            'wheel_slip_rear_left', 'wheel_slip_rear_right']].mean(axis=1)
+    df["mean_slip_1"] = np.clip(df["mean_slip_1"], 0, 2)
+
+    df["mean_slip_1"] = np.where(df["mean_slip_1"] > 0.6, 0.6, df["mean_slip_1"])
+
+
+    # Indice temporale per ogni sessione
+    df["time_index"] = df.groupby(['driver', 'track', 'temp']).cumcount()
+
+    # Media dello slip per ogni indice temporale su tutte le sessioni
+    mean_slip_over_time = df.groupby("time_index")["mean_slip_1"].mean().reset_index()
+
+    # Aggiungiamo la media mobile
+    window_size = 500  # Puoi modificarlo
+    mean_slip_over_time["rolling_mean"] = mean_slip_over_time["mean_slip_1"].rolling(window=window_size, center=True).mean()
+    # mean_slip_over_time["rolling_mean"] = mean_slip_over_time["rolling_mean"] + (mean_slip_over_time["rolling_mean"] * 1)
+
+
+    # Plot
+    plt.figure(figsize=(12, 6))
+    # plt.plot(mean_slip_over_time["time_index"], mean_slip_over_time["mean_slip_1"], label="Slip medio globale", alpha=0.4)
+    plt.plot(mean_slip_over_time["time_index"], mean_slip_over_time["rolling_mean"], label=f"Media mobile ({window_size})")
+    plt.title("Trend of global average slip over time with moving average")
+    plt.xlabel("Time index")
+    plt.ylabel("Average slip")
+    plt.grid(True)
+    plt.legend()
+    plt.tight_layout()
+    plt.show()
+
+
+
 
 def main():
     df = load_telemetry_data("dataset/vehicle_telemetry_*.csv")
--- data/data_analysis_single.py
+++ data/data_analysis_single.py
@@ -51,154 +51,154 @@ corr_matrix = df[important_vars + ["normalized_car_position"] + ["steer"]].corr(
 # Clipping outliers in all physical variables
 df = clip_outliers_iqr(df, columns)
 
-# # Plot Distributions of Important Variables To Understand the natural spread and potential abnormalities
-# df[important_vars].hist(bins=30, figsize=(15, 10))
-# plt.suptitle("Distribution of Key Physical Variables", fontsize=16)
-# plt.tight_layout()
-# plt.show()
+# Plot Distributions of Important Variables To Understand the natural spread and potential abnormalities
+df[important_vars].hist(bins=30, figsize=(15, 10))
+plt.suptitle("Distribution of Key Physical Variables", fontsize=16)
+plt.tight_layout()
+plt.show()
 
-# # Correlation Matrix Heatmap to understand relationships between forces, control inputs, and speed
-# sns.heatmap(corr_matrix, annot=True, fmt=".2f", cmap="coolwarm")
-# plt.title("Correlation Matrix of Key Variables")
-# plt.show()
+# Correlation Matrix Heatmap to understand relationships between forces, control inputs, and speed
+sns.heatmap(corr_matrix, annot=True, fmt=".2f", cmap="coolwarm")
+plt.title("Correlation Matrix of Key Variables")
+plt.show()
 
-# # Tire Temperature Analysis to see mean temperature across four tires
-# tyre_temp_cols = [
-#     "tyre_temp_front_left", "tyre_temp_front_right",
-#     "tyre_temp_rear_left", "tyre_temp_rear_right"
-# ]
-# df["avg_tyre_temp"] = df[tyre_temp_cols].mean(axis=1)
+# Tire Temperature Analysis to see mean temperature across four tires
+tyre_temp_cols = [
+    "tyre_temp_front_left", "tyre_temp_front_right",
+    "tyre_temp_rear_left", "tyre_temp_rear_right"
+]
+df["avg_tyre_temp"] = df[tyre_temp_cols].mean(axis=1)
 
-# # Overall distribution of tire temperatures
-# plt.figure(figsize=(10,6))
-# sns.histplot(df["avg_tyre_temp"], kde=True)
-# plt.title("Distribution of Average Tire Temperature °C")
-# plt.show()
+# Overall distribution of tire temperatures
+plt.figure(figsize=(10,6))
+sns.histplot(df["avg_tyre_temp"], kde=True)
+plt.title("Distribution of Average Tire Temperature °C")
+plt.show()
 
-# # Tire temperature segmented by grip classification
-# plt.figure(figsize=(10,6))
-# sns.boxplot(data=df, x="result", y="avg_tyre_temp")
-# plt.title("Average Tire Temperature by Grip Class")
-# plt.show()
+# Tire temperature segmented by grip classification
+plt.figure(figsize=(10,6))
+sns.boxplot(data=df, x="result", y="avg_tyre_temp")
+plt.title("Average Tire Temperature by Grip Class")
+plt.show()
 
-# # Wheel Slip Analysis to slip averages between front and rear tires
-# df["slip_front_mean"] = (df["wheel_slip_front_left"] + df["wheel_slip_front_right"]) / 2
-# df["slip_rear_mean"] = (df["wheel_slip_rear_left"] + df["wheel_slip_rear_right"]) / 2
-# df["slip_mean"] = (df["slip_front_mean"] + df["slip_rear_mean"]) / 2
+# Wheel Slip Analysis to slip averages between front and rear tires
+df["slip_front_mean"] = (df["wheel_slip_front_left"] + df["wheel_slip_front_right"]) / 2
+df["slip_rear_mean"] = (df["wheel_slip_rear_left"] + df["wheel_slip_rear_right"]) / 2
+df["slip_mean"] = (df["slip_front_mean"] + df["slip_rear_mean"]) / 2
 
-# # Compare slip distributions front vs rear
-# plt.figure(figsize=(14,6))
-# sns.histplot(df["slip_front_mean"], kde=True, color='blue', label='Front Slip')
-# sns.histplot(df["slip_rear_mean"], kde=True, color='red', label='Rear Slip')
-# plt.title("Distribution of Front vs Rear Wheel Slip")
-# plt.xlabel("Wheel Slip Value")
-# plt.legend()
-# plt.grid()
-# plt.show()
+# Compare slip distributions front vs rear
+plt.figure(figsize=(14,6))
+sns.histplot(df["slip_front_mean"], kde=True, color='blue', label='Front Slip')
+sns.histplot(df["slip_rear_mean"], kde=True, color='red', label='Rear Slip')
+plt.title("Distribution of Front vs Rear Wheel Slip")
+plt.xlabel("Wheel Slip Value")
+plt.legend()
+plt.grid()
+plt.show()
 
-# # Front slip classified by grip
-# plt.figure(figsize=(12,6))
-# sns.boxplot(x="result", y="slip_front_mean", data=df)
-# plt.title("Front Wheel Slip by Grip Classification")
-# plt.grid()
-# plt.show()
+# Front slip classified by grip
+plt.figure(figsize=(12,6))
+sns.boxplot(x="result", y="slip_front_mean", data=df)
+plt.title("Front Wheel Slip by Grip Classification")
+plt.grid()
+plt.show()
 
-# # Rear slip classified by grip
-# plt.figure(figsize=(12,6))
-# sns.boxplot(x="result", y="slip_rear_mean", data=df)
-# plt.title("Rear Wheel Slip by Grip Classification")
-# plt.grid()
-# plt.show()
+# Rear slip classified by grip
+plt.figure(figsize=(12,6))
+sns.boxplot(x="result", y="slip_rear_mean", data=df)
+plt.title("Rear Wheel Slip by Grip Classification")
+plt.grid()
+plt.show()
 
-# # Tire Pressure Analysis
-# df["pressure_front_mean"] = (df["pressure_front_left"] + df["pressure_front_right"]) / 2
-# df["pressure_rear_mean"] = (df["pressure_rear_left"] + df["pressure_rear_right"]) / 2
+# Tire Pressure Analysis
+df["pressure_front_mean"] = (df["pressure_front_left"] + df["pressure_front_right"]) / 2
+df["pressure_rear_mean"] = (df["pressure_rear_left"] + df["pressure_rear_right"]) / 2
 
-# # Distribution of front vs rear tire pressure
-# plt.figure(figsize=(14,6))
-# sns.histplot(df["pressure_front_mean"], kde=True, color='green', label='Front Pressure')
-# sns.histplot(df["pressure_rear_mean"], kde=True, color='orange', label='Rear Pressure')
-# plt.title("Distribution of Front vs Rear Tire Pressure PSI")
-# plt.xlabel("Pressure")
-# plt.legend()
-# plt.grid()
-# plt.show()
+# Distribution of front vs rear tire pressure
+plt.figure(figsize=(14,6))
+sns.histplot(df["pressure_front_mean"], kde=True, color='green', label='Front Pressure')
+sns.histplot(df["pressure_rear_mean"], kde=True, color='orange', label='Rear Pressure')
+plt.title("Distribution of Front vs Rear Tire Pressure PSI")
+plt.xlabel("Pressure")
+plt.legend()
+plt.grid()
+plt.show()
 
-# # Front pressure classified by grip
-# plt.figure(figsize=(12,6))
-# sns.boxplot(x="result", y="pressure_front_mean", data=df)
-# plt.title("Front Tire Pressure by Grip Classification")
-# plt.grid()
-# plt.show()
+# Front pressure classified by grip
+plt.figure(figsize=(12,6))
+sns.boxplot(x="result", y="pressure_front_mean", data=df)
+plt.title("Front Tire Pressure by Grip Classification")
+plt.grid()
+plt.show()
 
-# # Tire Temperature Detailed Front vs Rear
-# df["tyre_temp_front_mean"] = (df["tyre_temp_front_left"] + df["tyre_temp_front_right"]) / 2
-# df["tyre_temp_rear_mean"] = (df["tyre_temp_rear_left"] + df["tyre_temp_rear_right"]) / 2
-# plt.figure(figsize=(14,6))
-# sns.histplot(df["tyre_temp_front_mean"], kde=True, color='purple', label='Front Tire Temp')
-# sns.histplot(df["tyre_temp_rear_mean"], kde=True, color='pink', label='Rear Tire Temp')
-# plt.title("Distribution of Front vs Rear Tire Temperatures °C")
-# plt.xlabel("Temperature (°C)")
-# plt.legend()
-# plt.grid()
-# plt.show()
+# Tire Temperature Detailed Front vs Rear
+df["tyre_temp_front_mean"] = (df["tyre_temp_front_left"] + df["tyre_temp_front_right"]) / 2
+df["tyre_temp_rear_mean"] = (df["tyre_temp_rear_left"] + df["tyre_temp_rear_right"]) / 2
+plt.figure(figsize=(14,6))
+sns.histplot(df["tyre_temp_front_mean"], kde=True, color='purple', label='Front Tire Temp')
+sns.histplot(df["tyre_temp_rear_mean"], kde=True, color='pink', label='Rear Tire Temp')
+plt.title("Distribution of Front vs Rear Tire Temperatures °C")
+plt.xlabel("Temperature (°C)")
+plt.legend()
+plt.grid()
+plt.show()
 
-# # Front tire temperature by grip classification
-# plt.figure(figsize=(12,6))
-# sns.boxplot(x="result", y="tyre_temp_front_mean", data=df)
-# plt.title("Front Tire Temperature by Grip Classification")
-# plt.grid()
-# plt.show()
+# Front tire temperature by grip classification
+plt.figure(figsize=(12,6))
+sns.boxplot(x="result", y="tyre_temp_front_mean", data=df)
+plt.title("Front Tire Temperature by Grip Classification")
+plt.grid()
+plt.show()
 
-# custom_palette = {
-#     "grip_loss": "red",
-#     "neutral": "darkgreen",
-#     "high_grip_accelerate": "lightgreen",
-#     "low_grip_accelerate": "orange"
-# }
+custom_palette = {
+    "grip_loss": "red",
+    "neutral": "darkgreen",
+    "high_grip_accelerate": "lightgreen",
+    "low_grip_accelerate": "orange"
+}
 
-# # Tire Pressure vs Tire Temperature The Relationship between pressure and temperature
-# plt.figure(figsize=(12,6))
-# sns.scatterplot(x="pressure_rear_mean", y="tyre_temp_rear_mean", hue="result", data=df, palette=custom_palette)
-# plt.title("Rear Tire Pressure vs Rear Tire Temperature by Grip Classification")
-# plt.xlabel("Pressure")
-# plt.ylabel("Temperature °C")
-# plt.grid()
-# plt.show()
+# Tire Pressure vs Tire Temperature The Relationship between pressure and temperature
+plt.figure(figsize=(12,6))
+sns.scatterplot(x="pressure_rear_mean", y="tyre_temp_rear_mean", hue="result", data=df, palette=custom_palette)
+plt.title("Rear Tire Pressure vs Rear Tire Temperature by Grip Classification")
+plt.xlabel("Pressure")
+plt.ylabel("Temperature °C")
+plt.grid()
+plt.show()
 
-# # Speed Analysis, in particular the speed distribution by grip label
-# plt.figure(figsize=(12,6))
-# sns.boxplot(x="result", y="speed", data=df)
-# plt.title("Vehicle Speed by Grip Classification")
-# plt.grid()
-# plt.show()
+# Speed Analysis, in particular the speed distribution by grip label
+plt.figure(figsize=(12,6))
+sns.boxplot(x="result", y="speed", data=df)
+plt.title("Vehicle Speed by Grip Classification")
+plt.grid()
+plt.show()
 
-# # Speed along the track position (0=start, 1=end)
-# plt.figure(figsize=(12,6))
-# sns.scatterplot(x="normalized_car_position", y="speed", hue="result", data=df, palette=custom_palette)
-# plt.title("Speed along the Track vs Grip Classification")
-# plt.xlabel("Normalized Track Position")
-# plt.ylabel("Speed (km/h)")
-# plt.grid()
-# plt.show()
+# Speed along the track position (0=start, 1=end)
+plt.figure(figsize=(12,6))
+sns.scatterplot(x="normalized_car_position", y="speed", hue="result", data=df, palette=custom_palette)
+plt.title("Speed along the Track vs Grip Classification")
+plt.xlabel("Normalized Track Position")
+plt.ylabel("Speed (km/h)")
+plt.grid()
+plt.show()
 
-# # Lateral G-Force analysis across track
-# plt.figure(figsize=(12,6))
-# sns.scatterplot(x="normalized_car_position", y="g_force_y", hue="result", data=df, palette=custom_palette)
-# plt.title("Lateral G-Force along the Track by Grip Classification")
-# plt.xlabel("Normalized Track Position")
-# plt.ylabel("Lateral G-Force (g)")
-# plt.grid()
-# plt.show()
+# Lateral G-Force analysis across track
+plt.figure(figsize=(12,6))
+sns.scatterplot(x="normalized_car_position", y="g_force_y", hue="result", data=df, palette=custom_palette)
+plt.title("Lateral G-Force along the Track by Grip Classification")
+plt.xlabel("Normalized Track Position")
+plt.ylabel("Lateral G-Force (g)")
+plt.grid()
+plt.show()
 
-# # Mean Wheel Slip along the track
-# plt.figure(figsize=(12,6))
-# sns.scatterplot(x="normalized_car_position", y="slip_mean", hue="result", data=df, palette=custom_palette)
-# plt.title("Mean Wheel Slip along the Track by Grip Classification")
-# plt.xlabel("Normalized Track Position")
-# plt.ylabel("Mean Wheel Slip")
-# plt.grid()
-# plt.show()
+# Mean Wheel Slip along the track
+plt.figure(figsize=(12,6))
+sns.scatterplot(x="normalized_car_position", y="slip_mean", hue="result", data=df, palette=custom_palette)
+plt.title("Mean Wheel Slip along the Track by Grip Classification")
+plt.xlabel("Normalized Track Position")
+plt.ylabel("Mean Wheel Slip")
+plt.grid()
+plt.show()
 
 
 # Calcolo dello slip medio per ogni record
